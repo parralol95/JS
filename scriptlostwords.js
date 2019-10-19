09d65361038ce5d975c005ff86f6e58be3aad377
parralol95 <56206610+parralol95@users.noreply.github.com>
2019-10-19 20:06:18 +0200

    scriptlostwords.js

---
+++ scriptlostwords.js
@@ -0,0 +1,250 @@
+var questions =[{
+"question": "Ambodexter",
+"option1": "One who takes bribes from both sides",
+"option2": "Mondrian",
+"option3": "Paul Klee",
+"option4": "Kasimir Malevič ",
+"answer": "1" },
+
+{
+"question": "Betrump",
+"option1": "to deceive, cheat or to elude",
+"option2": "Salvador Dali",
+"option3": "Theo van Deosburg",
+"option4": "Joan Miro",
+"answer": "1" },
+
+{
+"question": "Coney-catch",
+"option1": "Sonia Delaunay",
+"option2": "To swindle,to deceive,",
+"option3": "Arshile Gorky",
+"option4": "Mark Rothko",
+"answer": "2" },
+
+{
+"question": "Hugger-mugger",
+"option1": "Concealment; secretly",
+"option2": "Helen Frankenthaler",
+"option3": "Mikhail Larionov",
+"option4": "Piet Mondrian",
+"answer": "1" },
+
+{
+"question": "Nickum",
+"option1": "Willem de Kooning",
+"option2": "Mondrian",
+"option3": "A cheating or dishonest person",
+"option4": "Kasimir Malevič",
+"answer": "3" },
+               
+ {
+"question": "Quacksalver",
+"option1": "Robert Motherwell",
+"option2": "Mondrian",
+"option3": "Salvador Dali",
+"option4": "A person who dishonestly claims knowledge of or skill in medicine",
+"answer": "4" }, 
+
+{
+"question": "Rouker",
+"option1": "Lee Krasner",
+"option2": "A person who spreads tales or rumours",
+"option3": "Frank Stella",
+"option4": "Morris Louis",
+"answer": "2" }, 
+                
+{
+"question": "Man-millinery",
+"option1": "Franz Kline",
+"option2": "Theo van Deosburg",
+"option3": "Suggestive of male vanity or pomposity",
+"option4": "Robert Delaunay",
+"answer": "3" }, 
+                
+{
+"question": "Parget",
+"option1": "Kenneth Noland",
+"option2": "Mondrian",
+"option3": "Hans Arp",
+"option4": "To daub or plaster (the face or body) with powder or paint",
+"answer": "4" }, 
+                
+{
+"question": "Snout-fair",
+"option1": "Having a fair countenance; fair-faced, handsome",
+"option2": "Lybuv Papova",
+"option3": "Mondrian",
+"option4": "Kasimir Malevič",
+"answer": "1" }, 
+                
+{
+"question": "Slug-a-bed",
+"option1": "Mark Rothko",
+"option2": "Lybuv Papova",
+"option3": "Robert Motherwell",
+"option4": "One who lies long in bed through laziness",
+"answer": "4" }, 
+                
+{
+"question": "Losenger",
+"option1": "Hans Hofmann",
+"option2": "Georges Braque",
+"option3": "A false flatterer, a lying rascal, a deceiver",
+"option4": "Mondrian",
+"answer": "3" }, 
+                
+{
+"question": "Momist",
+"option1": "Robert Delaunay",
+"option2": "Paul Klee",
+"option3": "A person who habitually finds fault; a harsh critic",
+"option4": "Lee Krasner",
+"answer": "3" }, 
+ 
+{
+"question": "Peacockize",
+"option1": "Mikhail Larionov",
+"option2": "Kasimir Malevič",
+"option3": "To behave like a peacock; esp. to pose or strut ostentatiously",
+"option4": "Lybuv Papova",
+"answer": "3" }, 
+                
+{
+"question": "Percher",
+"option1": "A person who aspires to a higher rank or status; an ambitious or self-assertive person",
+"option2": "Lybuv Papova",
+"option3": "Joan Miro",
+"option4": "Hans Hofmann",
+"answer": "1" }, 
+                
+{
+"question": "Rouzy-bouzy",
+"option1": "Mikhail Larionov",
+"option2": "Boisterously drunk",
+"option3": "Ellsworth Kelly",
+"option4": "Gerhard Richter",
+"answer": "2" }, 
+                
+{
+"question": "Ruff",
+"option1": "Kenneth Noland",
+"option2": "Sam Francis",
+"option3": "Paul Klee",
+"option4": "To swagger, bluster, domineer. To ruff it out / to brag or boast of a thing",
+"answer": "4" }, 
+
+{
+"question": "Sillytonian",
+"option1": "A silly or gullible person",
+"option2": "Kasimir Malevič",
+"option3": "Paul Klee",
+"option4": "Lybuv Papova",
+"answer": "1" },
+                
+{
+"question": "Wlonk",
+"option1": "Wassily Kandinsky",
+"option2": "Kasimir Malevič",
+"option3": "Willem de Kooning",
+"option4": "Proud, haughty /  Rich, splendid, fine, magnificent",
+"answer": "4" },
+                
+{
+"question": "Fumish",
+"option1": "Inclined to fume, hot-tempered, irascible, passionate",
+"option2": "Morris Louis",
+"option3": "Juan Gris",
+"option4": "Lybuv Papova",
+"answer": "1" },
+                
+{
+"question": "Awhape",
+"option1": "Franz Kline",
+"option2": "Barnett Newman",
+"option3": "To amaze, stupefy with fear, confound utterly",
+"option4": "Kenneth Noland",
+"answer": "3" },
+                
+{
+"question": "Hugge",
+"option1": "Georges Braque",
+"option2": "Salvador Dali",
+"option3": "To shudder, shrink, shiver, or shake with fear or with cold",
+"option4": "Piet Mondrian",
+"answer": "3" },
+                
+{
+"question": "Merry-go-sorry",
+"option1": "Hans Arp",
+"option2": "Frank Stella",
+"option3": "To shudder, shrink, shiver, or shake with fear or with cold",
+"option4": "Ellsworth Kelly",
+"answer": "3" }, 
+                
+{
+"question": "Stomaching",
+"option1": "Arshile Gorky",
+"option2": "Robert Motherwell",
+"option3": "Full of malignity; given to cherish anger or resentment",
+"option4": "Sam Francis",
+"answer": "3" },
+                
+{
+"question": "Swerk",
+"option1": "Hans Arp",
+"option2": "to become dark or gloomy, troubled, sad",
+"option3": "Vasil'evič Kandinskij",
+"option4": "Josef Albers",
+"answer": "2" },
+                
+{
+"question": "Teen",
+"option1": "Jackson Pollock",
+"option2": "To vex, irritate, annoy, anger, enrage / To inflict suffering upon",
+"option3": "Kazimir Malevič",
+"option4": "Paul Klee",
+"answer": "2" },
+                
+{
+"question": "Tremblable",
+"option1": "Juan Gris",
+"option2": "Causing dread or horror",
+"option3": "Georges Braque",
+"option4": "Robert Motherwell",
+"answer": "2" },
+                
+{
+"question": "Westeheart",
+"option1": "Franz Kline",
+"option2": "Lee Krasner",
+"option3": "Used to express grief, pity, regret, disappointment, or concern, like wasteheart-a-day, wasteheart of me",
+"option4": "Willem de Kooning",
+"answer": "3" },
+                
+{
+"question": "Dowsabel",
+"option1": "Mikhail Larionov",
+"option2": "Helen Frankenthaler",
+"option3": "Salvador Dali",
+"option4": "Applied generically to a sweetheart, ‘lady-love",
+"answer": "4" },
+
+{
+"question": "Ear-rent",
+"option1": "The figurative cost to a person of listening to trivial or incessant talk",
+"option2": "Frank Stella",
+"option3": "Ellsworth Kelly",
+"option4": "Kenneth Noland",
+"answer": "1" },
+
+
+{
+"question": "Doniferous",
+"option1": "Bearing gifts",
+"option2": "Jackosn Pollock",
+"option3": "Piet Mondrian",
+"option4": "Mikhail Larionov",
+"answer": "1" }
+
+]
