652aa451d02a7f09d766d6018545dbb706592742
parralol95 <56206610+parralol95@users.noreply.github.com>
2019-10-21 21:00:35 +0200

    scriptlostwords;.js

---
+++ scriptlostwords;.js
@@ -0,0 +1,251 @@
+
+var questions =[{
+"question": "Ambodexter",
+"option1": "One who takes bribes from both sides",
+"option2": "One who is skilled at working with hands",
+"option3": "Someone who has a bipolar behaviour",
+"option4": "One who takes part in an activity if it is convenient",
+"answer": "1" },
+
+{
+"question": "Betrump",
+"option1": "to deceive, cheat or to elude",
+"option2": "to defeat by playing cards",
+"option3": "to work intensively",
+"option4": "to overtake an opponent",
+"answer": "1" },
+
+{
+"question": "Coney-catch",
+"option1": "to earn honestly",
+"option2": "To swindle,to deceive,",
+"option3": "to keep something hidden",
+"option4": "to catch a rabbit",
+"answer": "2" },
+
+{
+"question": "Hugger-mugger",
+"option1": "Concealment; secretly",
+"option2": "A person who keep a mug in his hands",
+"option3": "One who hug people",
+"option4": "Reconciliation",
+"answer": "1" },
+
+{
+"question": "Nickum",
+"option1": "A place where people can share their problems",
+"option2": "A reliable person",
+"option3": "A cheating or dishonest person",
+"option4": "One who tell the truth",
+"answer": "3" },
+               
+ {
+"question": "Quacksalver",
+"option1": "One who quickly saves lives",
+"option2": "An animal which quack",
+"option3": "A burglar alarm",
+"option4": "A person who dishonestly claims knowledge of or skill in medicine",
+"answer": "4" }, 
+
+{
+"question": "Rouker",
+"option1": "One who speaks aloud",
+"option2": "A person who spreads tales or rumours",
+"option3": "One who speaks about tales",
+"option4": "One who make up stories",
+"answer": "2" }, 
+                
+{
+"question": "Man-millinery",
+"option1": "A man who sells millinery",
+"option2": "A particular man's hat",
+"option3": "Suggestive of male vanity or pomposity",
+"option4": "handsome and generous",
+"answer": "3" }, 
+                
+{
+"question": "Parget",
+"option1": "to remove a stain",
+"option2": "to whitewash the wall",
+"option3": "to dress appropriately",
+"option4": "To daub or plaster (the face or body) with powder or paint",
+"answer": "4" }, 
+                
+{
+"question": "Snout-fair",
+"option1": "Having a fair countenance; fair-faced, handsome",
+"option2": "having an ugly wax",
+"option3": "A good cigarette smoke",
+"option4": "Having a perfect face to be an actor",
+"answer": "1" }, 
+                
+{
+"question": "Slug-a-bed",
+"option1": "A bed that is as soft as a slug",
+"option2": "A person who sleep a lot",
+"option3": "One who moves as slowly as a slug",
+"option4": "One who lies long in bed through laziness",
+"answer": "4" }, 
+                
+{
+"question": "Losenger",
+"option1": "someone who sucks a lot of sweets",
+"option2": "Someone who makes truthful compliments",
+"option3": "A false flatterer, a lying rascal, a deceiver",
+"option4": "A person who has lost his or her temper",
+"answer": "3" }, 
+                
+{
+"question": "Momist",
+"option1": "One who talks about someone in a bad way",
+"option2": "A person who doesn't care about criticism",
+"option3": "A person who habitually finds fault; a harsh critic",
+"option4": "One who is particulary tied to his or her mother",
+"answer": "3" }, 
+ 
+{
+"question": "Peacockize",
+"option1": "To behave like a rabbit, hiding because of fear",
+"option2": "Being in a bad mood",
+"option3": "To behave like a peacock, to pose ostentatiously",
+"option4": "One who shows himself or herself at his or her worst",
+"answer": "3" }, 
+                
+{
+"question": "Percher",
+"option1": "A person who aspires to a higher rank or status",
+"option2": "One who has no ambitions",
+"option3": "An animal which sits on the edge of the perch",
+"option4": "A persone who waits for the inheritance  ",
+"answer": "1" }, 
+                
+{
+"question": "Rouzy-bouzy",
+"option1": "Someone who has an annoying behaviour",
+"option2": "When someone is boisterously drunk",
+"option3": "One who behaves roughly",
+"option4": "When someone is drunk rotten",
+"answer": "2" }, 
+                
+{
+"question": "Ruff",
+"option1": "To be worried about someone",
+"option2": "To bother someone who is busy",
+"option3": "A collar worn in Europe in the 16th and 17th centuries",
+"option4": "To swagger, domineer, to brag or boast of a thing",
+"answer": "4" }, 
+
+{
+"question": "Sillytonian",
+"option1": "A silly or gullible person",
+"option2": "A smart and strategic person",
+"option3": "A forgetful person",
+"option4": "A person who makes terrible choices",
+"answer": "1" },
+                
+{
+"question": "Wlonk",
+"option1": "good looking, beautiful",
+"option2": "Smart, quick ",
+"option3": "Interesting",
+"option4": "Proud, haughty, magnificent",
+"answer": "4" },
+                
+{
+"question": "Fumish",
+"option1": "Inclined to fume, hot-tempered, irascible, passionate",
+"option2": "Inclined to keep calm, to be easy-going",
+"option3": "to be confused, disoriented",
+"option4": "to be messed up",
+"answer": "1" },
+                
+{
+"question": "Awhape",
+"option1": "to get a blinding flash",
+"option2": "to wake someone up",
+"option3": "To amaze, stupefy with fear, confound utterly",
+"option4": "to make sounds to express joy",
+"answer": "3" },
+                
+{
+"question": "Hugge",
+"option1": "Someone who like to hug and cuddles",
+"option2": "to cuddle, embrace, cuddle",
+"option3": "To shudder, shrink, shiver, or shake with fear or with cold",
+"option4": "to be friendly, lovely",
+"answer": "3" },
+                
+{
+"question": "Merry-go-sorry",
+"option1": "To decline a proposal",
+"option2": "A celebration has ended badly",
+"option3": "To shudder or shake with fear or with cold",
+"option4": "A large machine at a fair that turns round",
+"answer": "3" }, 
+                
+{
+"question": "Stomaching",
+"option1": "Feeling sad and depressed",
+"option2": "Getting a stomac ache",
+"option3": "Full of malignity,anger or resentment",
+"option4": "Having abdominal pain beacuse of hunger",
+"answer": "3" },
+                
+{
+"question": "Swerk",
+"option1": "To exclaim something awful   ",
+"option2": "To become dark or gloomy, troubled, sad",
+"option3": "To get hungry",
+"option4": "To change direction suddenly",
+"answer": "2" },
+                
+{
+"question": "Teen",
+"option1": "To jeopardize something",
+"option2": "To vex, irritate, annoy, anger, enrage / To inflict suffering upon",
+"option3": "To play with other kids",
+"option4": "To behave like a kid",
+"answer": "2" },
+                
+{
+"question": "Tremblable",
+"option1": "Something that is cold and makes tremble",
+"option2": "Causing dread or horror",
+"option3": "An illness that makes tremble",
+"option4": "Likely to break, fragile",
+"answer": "2" },
+                
+{
+"question": "Westeheart",
+"option1": "A person who is rude and heartless",
+"option2": "A person who is kind and generous",
+"option3": "Used to express grief, pity, regret, disappointment, or concern, like wasteheart-a-day, wasteheart of me",
+"option4": "Used to express joyful thoughts about life and people ",
+"answer": "3" },
+                
+{
+"question": "Dowsabel",
+"option1": "Often referred to a generous person",
+"option2": "Applied generically to an animal",
+"option3": "Often referred to a rude person",
+"option4": "Applied generically to a sweetheart, ‘lady-love",
+"answer": "4" },
+
+{
+"question": "Ear-rent",
+"option1": "The figurative cost to a person of listening to trivial or incessant talk",
+"option2": "To pay attention to other people's conversation",
+"option3": "To pay attention to someone who is talking",
+"option4": "behaviour that consists in repeating a sentence in order to make the concept clear",
+"answer": "1" },
+
+
+{
+"question": "Doniferous",
+"option1": "Bearing gifts",
+"option2": "Attached",
+"option3": "polite, well-behaved",
+"option4": "curious",
+"answer": "1" }
+
+]
