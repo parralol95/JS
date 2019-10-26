66d591726242db9e48cab415ac18ff0ee9bb0ec3
parralol95 <56206610+parralol95@users.noreply.github.com>
2019-10-26 18:03:55 +0200

    script.medievalpaintings.js

---
+++ script.medievalpaintings.js
@@ -0,0 +1,250 @@
+var questions =[{
+"question": "Ognissanti Madonna, 1306, Ognissanti Church, Florence",
+"option1": "Giotto",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" },
+
+{
+"question": "Good Sherpherd,mosaic, Mausoleum of Galla Placidia, Ravenna, 5th century CE",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "Anonymous",
+"answer": "4" },
+
+{
+"question": "The Bury Bible,1130-1135, Bury St Edmunds, England, Corpus Christi College, MS 2, fol. 94",
+"option1": "",
+"option2": "Master Hugo",
+"option3": "",
+"option4": "",
+"answer": "2" },
+
+{
+"question": "Apse painting, Sant Clement in Taüll, 1123 (Museu Nacional d'Art de Catalunya, Barcelona)",
+"option1": "Master of Taüll ",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" },
+
+{
+"question": "Santa Trinità Maestà, 1280-1285,Uffizi Gallery, Florence",
+"option1": "",
+"option2": "",
+"option3": "Cimabue",
+"option4": "",
+"answer": "3" },
+               
+ {
+"question": "Maestà, 1308-1311, Siena Cathedral's museum ",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "Duccio di Buoninsegna",
+"answer": "4" }, 
+
+{
+"question": "Lamentation, 1306, Scrovegni Chapel, Padua",
+"option1": "",
+"option2": "Giotto",
+"option3": "",
+"option4": "",
+"answer": "2" }, 
+                
+{
+"question": "Crucifix, 1288, Santa Croce, Florence",
+"option1": "",
+"option2": "",
+"option3": "Cimabue",
+"option4": "",
+"answer": "3" }, 
+                
+{
+"question": "Allegory of the Good Government, 1338-40, Frescoes, Public Palace, Siena",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "Ambrogio Lorenzetti",
+"answer": "4" }, 
+                
+{
+"question": "Crucifixion of Rimini, 1310-1317, Malatesta Temple, Rimini",
+"option1": "Giotto",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" }, 
+                
+{
+"question": "Rucellai Madonna, 1285, Uffizi Gallery, Florence ",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "Duccio di Buoninsegna",
+"answer": "4" }, 
+                
+{
+"question": "Virgin Mary and Child, Louvre Museum, 1354.",
+"option1": "",
+"option2": "",
+"option3": "Simone Martini",
+"option4": "",
+"answer": "3" }, 
+                
+{
+"question": "The Flagellation of Christ, 1280, The Frick Collection",
+"option1": "",
+"option2": "",
+"option3": "Cimabue",
+"option4": "",
+"answer": "3" }, 
+ 
+{
+"question": "Annunciation, 1308-1311, London National Gallery",
+"option1": "",
+"option2": "",
+"option3": "Duccio di Buoninsegna",
+"option4": "",
+"answer": "3" }, 
+                
+{
+"question": "The Birth of Mary, 1342, Siena Cathedral's museum",
+"option1": "Pietro lorenzetti",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" }, 
+                
+{
+"question": "Pentecost, Staatliche Museen, Berlin, 1335-1340 ",
+"option1": "",
+"option2": "Taddeo Gaddi",
+"option3": "",
+"option4": "",
+"answer": "2" }, 
+                
+{
+"question": "Virgin Mary and Child, Louvre Museum, 1354",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "Paolo Veneziano",
+"answer": "4" }, 
+
+{
+"question": "Predella panel: Hermits at the Fountain of Elijah, Siena Gallery, 1328-1329",
+"option1": "Pietro Lorenzetti",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" },
+                
+{
+"question": "Belleville Breviary",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "Jean Pucelle",
+"answer": "4" },
+                
+{
+"question": "Madonna with child, 1283, Museum of Santa Verdiana",
+"option1": "Cimabue",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" },
+                
+{
+"question": "Annunciation, 1344, National Museum of Siena",
+"option1": "",
+"option2": "",
+"option3": "Ambrogio Lorenzetti",
+"option4": "",
+"answer": "3" },
+                
+{
+"question": " January, Les Très Riches Heures du Duc de Berry, illumination, 1413-16, (Musée Condé, Chantilly)",
+"option1": "",
+"option2": "",
+"option3": "Limbourg brothers",
+"option4": "",
+"answer": "3" },
+                
+{
+"question": "Virgin and Child with Saints, 1325, Isabella Stewart Gardner Museum ",
+"option1": "",
+"option2": "",
+"option3": "Simone Martini",
+"option4": "",
+"answer": "3" }, 
+                
+{
+"question": "Universal Man, 1165, illustration from Liber Divinorum Operum",
+"option1": "",
+"option2": "",
+"option3": "Hildegard Von Bingen",
+"option4": "",
+"answer": "3" },
+                
+{
+"question": "Allegory of Bad Governement, 1338-1340, Frescoes, Public Palace, Siena",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "2" },
+                
+{
+"question": "Life of Christ, 1304-1306, frescoes, Scrovegni Chapel, Padua",
+"option1": ""
+"option2": "Giotto",
+"option3": "",
+"option4": "",
+"answer": "2" },
+                
+{
+"question": "Scenes of the Life of St Nicholas, 1332, Uffizi Gallery, Florence",
+"option1": "",
+"option2": "Ambrogio Lorenzetti",
+"option3": "",
+"option4": "",
+"answer": "2" },
+                
+{
+"question": "Saint Catherine of Alexandria Polyptych or Pisa Polyptych, 1320, National Museum of San Matteo",
+"option1": "",
+"option2": "",
+"option3": "Simone Martini",
+"option4": "",
+"answer": "3" },
+                
+{
+"question": "Allegory of Gluttony and Lust, Yale University Art Gallery, New Haven",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "BOSCH, Hieronymus",
+"answer": "4" },
+
+{
+"question": "Legend of St. Francis, frescoes, 1297-1299, Upper Church, St. Francis, Assisi",
+"option1": "Giotto",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" },
+
+
+{
+"question": "Petrarch's Virgil, 1336, illustration, the Ambrosian Art Gallery, Milan, Italy",
+"option1": "Simone Martini",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "1" }
+
+]
