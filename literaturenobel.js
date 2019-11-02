97daacfae112f6373a66600d03ee510db6142096
parralol95 <56206610+parralol95@users.noreply.github.com>
2019-11-02 15:18:32 +0100

    literaturenobel.js

---
+++ literaturenobel.js
@@ -0,0 +1,250 @@
+var questions =[{
+"question": "1926, Ashes, novel",
+"option1": "Grazia Deledda",
+"option2": "Saul Bellow",
+"option3": "isaac bashevis singer",
+"option4": "Toni Morrison",
+"answer": "1" },
+
+{
+"question": "2019, A Sorrow Beyond Dreams, novel",
+"option1": "Peter Handke",
+"option2": "Bob Dylan",
+"option3": "",
+"option4": "wole soyinka",
+"answer": "1" },
+
+{
+"question": "1901, Justice, poem",
+"option1": "Mario Vargas Llosa",
+"option2": "Sully Prudhomme",
+"option3": "J. M. G. Le Clézio",
+"option4": "Olga Tokarczuk",
+"answer": "2" },
+
+{
+"question": "1925, The Doctor's Dilemma, novel",
+"option1": "George Bernard Shaw",
+"option2": "",
+"option3": "Saul Bellow",
+"option4": "isaac bashevis singer",
+"answer": "1" },
+
+{
+"question": "1934, One, No One and One Hundred Thousand, novel",
+"option1": "Mario Vargas Llosa",
+"option2": "Toni Morrison",
+"option3": "Luigi Pirandello",
+"option4": "",
+"answer": "3" },
+               
+ {
+"question": "1929, The Magic Mountain, novel",
+"option1": "Bob Dylan",
+"option2": "wole soyinka",
+"option3": "Peter Handke",
+"option4": "Thomas Mann",
+"answer": "4" }, 
+
+{
+"question": "1946, Narcissus and Goldmund, novel",
+"option1": "Toni Morrison",
+"option2": "Hermann Hesse",
+"option3": "",
+"option4": "Saul Bellow",
+"answer": "2" }, 
+                
+{
+"question": "1948, The Love Song of J. Alfred Prufrock, poem",
+"option1": "J. M. G. Le Clézio",
+"option2": "wole soyinka",
+"option3": "T.S.Eliot",
+"option4": "",
+"answer": "3" }, 
+                
+{
+"question": "1907, The Jungle Book, novel",
+"option1": "",
+"option2": "",
+"option3": "Saul Bellow",
+"option4": "Rudyard Kipling",
+"answer": "4" }, 
+                
+{
+"question": "1938, The Good Earth, novel",
+"option1": "Pearl S.Buck",
+"option2": "isaac bashevis singer",
+"option3": "",
+"option4": "Bob Dylan",
+"answer": "1" }, 
+                
+{
+"question": "1945, Give Me Your Hand, poem",
+"option1": "",
+"option2": "Mario Vargas Llosa",
+"option3": "Saul Bellow",
+"option4": "Gabriela Mistral",
+"answer": "4" }, 
+                
+{
+"question": "1950, Principia Mathematica, essay",
+"option1": "",
+"option2": "Toni Morrison",
+"option3": "Berstrand Russell",
+"option4": "Olga Tokarczuk",
+"answer": "3" }, 
+                
+{
+"question": "1966, Glühende Rätsel II(Glowing Puzzles II), poem,
+"option1": "",
+"option2": "",
+"option3": "Nelly Sachs",
+"option4": "",
+"answer": "3" }, 
+ 
+{
+"question": "1968, The Dancing Girl of Izu, novel",
+"option1": "isaac bashevis singer",
+"option2": "J. M. G. Le Clézio",
+"option3": "Yasunari Kawabata",
+"option4": "Saul Bellow",
+"answer": "3" }, 
+                
+{
+"question": "1971, Twenty Love Poems and a Song of Despair, poetry collection",
+"option1": "Pablo Neruda",
+"option2": "wole soyinka",
+"option3": "J. M. G. Le Clézio",
+"option4": "Peter Handke",
+"answer": "1" }, 
+                
+{
+"question": "1975, Cuttlefish Bones, poetry collection",
+"option1": "Mario Vargas Llosa",
+"option2": "Eugenio Montale",
+"option3": "",
+"option4": "Bob Dylan",
+"answer": "2" }, 
+                
+{
+"question": "1953, The world Crisis, non fiction novel",
+"option1": "Toni Morrison",
+"option2": "Mario Vargas Llosa",
+"option3": "Olga Tokarczuk",
+"option4": "Winston Churchill",
+"answer": "4" }, 
+
+{
+"question": "1964, Nausea, novel",
+"option1": "Jean-Paul Sartre",
+"option2": "isaac bashevis singer",
+"option3": "",
+"option4": "",
+"answer": "1" },
+                
+{
+"question": "1962, East of Eden, novel",
+"option1": "J. M. G. Le Clézio",
+"option2": "Saul Bellow",
+"option3": "wole soyinka",
+"option4": "John Steinbeck",
+"answer": "4" },
+                
+{
+"question": "1954, The Old Man and the Sea",
+"option1": "Ernest Hemingway",
+"option2": "Olga Tokarczuk",
+"option3": "",
+"option4": "Bob Dylan",
+"answer": "1" },
+                
+{
+"question": "1958, Doctor Zhivago, novel ",
+"option1": "Toni Morrison",
+"option2": "isaac bashevis singer",
+"option3": "Boris Pasternak",
+"option4": "wole soyinka",
+"answer": "3" },
+                
+{
+"question": "",
+"option1": "",
+"option2": "",
+"option3": "",
+"option4": "",
+"answer": "3" },
+                
+{
+"question": "Simultaneous Counter Composition., 1930",
+"option1": "Hans Arp",
+"option2": "Frank Stella",
+"option3": "Theo van Doesburg",
+"option4": "Ellsworth Kelly",
+"answer": "3" }, 
+                
+{
+"question": "Premier disque, 1912",
+"option1": "Arshile Gorky",
+"option2": "Robert Motherwell",
+"option3": "Robert Delaunay",
+"option4": "Sam Francis",
+"answer": "3" },
+                
+{
+"question": "Autumn Rhythm, 1950",
+"option1": "Hans Arp",
+"option2": "Jackson Pollock",
+"option3": "Vasil'evič Kandinskij",
+"option4": "Josef Albers",
+"answer": "2" },
+                
+{
+"question": "The Joy of Life, 1930",
+"option1": "Jackson Pollock",
+"option2": "Robert Delaunay",
+"option3": "Kazimir Malevič",
+"option4": "Paul Klee",
+"answer": "2" },
+                
+{
+"question": "Yellow Island, 1952",
+"option1": "Juan Gris",
+"option2": "Jackson Pollock",
+"option3": "Georges Braque",
+"option4": "Robert Motherwell",
+"answer": "2" },
+                
+{
+"question": "Composition VII (the three graces), 1917",
+"option1": "Franz Kline",
+"option2": "Lee Krasner",
+"option3": "The van Doesburg",
+"option4": "Willem de Kooning",
+"answer": "3" },
+                
+{
+"question": "Orange and Yellow, 1956",
+"option1": "Mikhail Larionov",
+"option2": "Helen Frankenthaler",
+"option3": "Salvador Dali",
+"option4": "Mark Rothko",
+"answer": "4" },
+
+{
+"question": "Nulli Secundus, 1964",
+"option1": "Hans Hofmann",
+"option2": "Frank Stella",
+"option3": "Ellsworth Kelly",
+"option4": "Kenneth Noland",
+"answer": "1" },
+
+
+{
+"question": "The Green Stripe, 1955",
+"option1": "Mark Rothko",
+"option2": "Jackosn Pollock",
+"option3": "Piet Mondrian",
+"option4": "Mikhail Larionov",
+"answer": "1" }
+
+]
