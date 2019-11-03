a4f09f9aa600cf0cc9de02a3adffa17c771c8101
parralol95 <56206610+parralol95@users.noreply.github.com>
2019-11-03 19:21:39 +0100

    literaturenobel.js
    finished

--- literaturenobel.js
+++ literaturenobel.js
@@ -1,5 +1,5 @@
 var questions =[{
-"question": "1926, Ashes, novel",
+"question": "1926, Dopo il Divorzio-After the Divorce(1902), novel",
 "option1": "Grazia Deledda",
 "option2": "Saul Bellow",
 "option3": "isaac bashevis singer",
@@ -7,15 +7,15 @@ var questions =[{
 "answer": "1" },
 
 {
-"question": "2019, A Sorrow Beyond Dreams, novel",
+"question": "2019, A Sorrow Beyond Dreams(1972), novel",
 "option1": "Peter Handke",
 "option2": "Bob Dylan",
-"option3": "",
+"option3": "José Saramago",
 "option4": "wole soyinka",
 "answer": "1" },
 
 {
-"question": "1901, Justice, poem",
+"question": "1901,La Justice-Justice(1878), poem",
 "option1": "Mario Vargas Llosa",
 "option2": "Sully Prudhomme",
 "option3": "J. M. G. Le Clézio",
@@ -23,23 +23,23 @@ var questions =[{
 "answer": "2" },
 
 {
-"question": "1925, The Doctor's Dilemma, novel",
+"question": "1925, The Doctor's Dilemma(1906), novel",
 "option1": "George Bernard Shaw",
-"option2": "",
+"option2": "Isaac Bashevis Singer",
 "option3": "Saul Bellow",
 "option4": "isaac bashevis singer",
 "answer": "1" },
 
 {
-"question": "1934, One, No One and One Hundred Thousand, novel",
+"question": "1934, One, No One and One Hundred Thousand(1926), novel",
 "option1": "Mario Vargas Llosa",
 "option2": "Toni Morrison",
 "option3": "Luigi Pirandello",
-"option4": "",
+"option4": "Imre Kertész",
 "answer": "3" },
                
  {
-"question": "1929, The Magic Mountain, novel",
+"question": "1929, The Magic Mountain(1924), novel",
 "option1": "Bob Dylan",
 "option2": "wole soyinka",
 "option3": "Peter Handke",
@@ -47,71 +47,71 @@ var questions =[{
 "answer": "4" }, 
 
 {
-"question": "1946, Narcissus and Goldmund, novel",
+"question": "1946, Narcissus and Goldmund(1930), novel",
 "option1": "Toni Morrison",
 "option2": "Hermann Hesse",
-"option3": "",
+"option3": "Derek Walcott",
 "option4": "Saul Bellow",
 "answer": "2" }, 
                 
 {
-"question": "1948, The Love Song of J. Alfred Prufrock, poem",
+"question": "1948, The Love Song of J. Alfred Prufrock(1915), poem",
 "option1": "J. M. G. Le Clézio",
 "option2": "wole soyinka",
 "option3": "T.S.Eliot",
-"option4": "",
+"option4": "Isaac Bashevis Singer",
 "answer": "3" }, 
                 
 {
-"question": "1907, The Jungle Book, novel",
-"option1": "",
-"option2": "",
+"question": "1907, The Jungle Book(1894), novel",
+"option1": "Elias Canetti",
+"option2": "Mo Yan",
 "option3": "Saul Bellow",
 "option4": "Rudyard Kipling",
 "answer": "4" }, 
                 
 {
-"question": "1938, The Good Earth, novel",
+"question": "1938, The Good Earth(1931), novel",
 "option1": "Pearl S.Buck",
 "option2": "isaac bashevis singer",
-"option3": "",
+"option3": "Kenzaburō Ōe",
 "option4": "Bob Dylan",
 "answer": "1" }, 
                 
 {
-"question": "1945, Give Me Your Hand, poem",
-"option1": "",
+"question": "1945, Sonetos de la Muerte- Sonnets of Death(1914), collection of poems",
+"option1": "Mo Yan",
 "option2": "Mario Vargas Llosa",
 "option3": "Saul Bellow",
 "option4": "Gabriela Mistral",
 "answer": "4" }, 
                 
 {
-"question": "1950, Principia Mathematica, essay",
-"option1": "",
+"question": "1950, A History of Western Philosophy and Its Connection with Political and Social Circumstances from the Earliest Times to the Present Day(1945), essay",
+"option1": "Imre Kertész",
 "option2": "Toni Morrison",
 "option3": "Berstrand Russell",
 "option4": "Olga Tokarczuk",
 "answer": "3" }, 
                 
 {
-"question": "1966, Glühende Rätsel II(Glowing Puzzles II), poem,
-"option1": "",
-"option2": "",
+"question": "1966, In den Wohnungen des Todes-In the House of Death(1947), collection of poems,
+"option1": "José Saramago",
+"option2": "Isaac Bashevis Singer",
 "option3": "Nelly Sachs",
-"option4": "",
+"option4": "Derek Walcott",
 "answer": "3" }, 
  
 {
-"question": "1968, The Dancing Girl of Izu, novel",
+"question": "1968, The Dancing Girl of Izu(1926), novel",
 "option1": "isaac bashevis singer",
-"option2": "J. M. G. Le Clézio",
+"option2": "J.M.G. Le Clézio",
 "option3": "Yasunari Kawabata",
 "option4": "Saul Bellow",
 "answer": "3" }, 
                 
 {
-"question": "1971, Twenty Love Poems and a Song of Despair, poetry collection",
+"question": "1971, Twenty Love Poems and a Song of Despair(1924), poetry collection",
 "option1": "Pablo Neruda",
 "option2": "wole soyinka",
 "option3": "J. M. G. Le Clézio",
@@ -119,15 +119,15 @@ var questions =[{
 "answer": "1" }, 
                 
 {
-"question": "1975, Cuttlefish Bones, poetry collection",
+"question": "1975, Cuttlefish Bones(1925), poetry collection",
 "option1": "Mario Vargas Llosa",
 "option2": "Eugenio Montale",
-"option3": "",
+"option3": "José Saramago",
 "option4": "Bob Dylan",
 "answer": "2" }, 
                 
 {
-"question": "1953, The world Crisis, non fiction novel",
+"question": "1953, The World Crisis(1923), non fiction novel",
 "option1": "Toni Morrison",
 "option2": "Mario Vargas Llosa",
 "option3": "Olga Tokarczuk",
@@ -135,15 +135,15 @@ var questions =[{
 "answer": "4" }, 
 
 {
-"question": "1964, Nausea, novel",
+"question": "1964, Nausea(1938), novel",
 "option1": "Jean-Paul Sartre",
 "option2": "isaac bashevis singer",
-"option3": "",
-"option4": "",
+"option3": "Kenzaburō Ōe",
+"option4": "Tomas Tranströmer",
 "answer": "1" },
                 
 {
-"question": "1962, East of Eden, novel",
+"question": "1962, East of Eden( 1952), novel",
 "option1": "J. M. G. Le Clézio",
 "option2": "Saul Bellow",
 "option3": "wole soyinka",
@@ -151,15 +151,15 @@ var questions =[{
 "answer": "4" },
                 
 {
-"question": "1954, The Old Man and the Sea",
+"question": "1954, The Old Man and the Sea(1952), novel",
 "option1": "Ernest Hemingway",
 "option2": "Olga Tokarczuk",
-"option3": "",
+"option3": "Svetlana Alexievich",
 "option4": "Bob Dylan",
 "answer": "1" },
                 
 {
-"question": "1958, Doctor Zhivago, novel ",
+"question": "1958, Doctor Zhivago(1966), novel ",
 "option1": "Toni Morrison",
 "option2": "isaac bashevis singer",
 "option3": "Boris Pasternak",
@@ -167,84 +167,84 @@ var questions =[{
 "answer": "3" },
                 
 {
-"question": "",
-"option1": "",
-"option2": "",
-"option3": "",
-"option4": "",
+"question": "1906, Barbarian Odes(1877), poetry",
+"option1": "Derek Walcott",
+"option2": "Tomas Tranströmer",
+"option3": "Giosuè Carducci",
+"option4": "Imre Kertész",
 "answer": "3" },
                 
 {
-"question": "Simultaneous Counter Composition., 1930",
-"option1": "Hans Arp",
-"option2": "Frank Stella",
-"option3": "Theo van Doesburg",
-"option4": "Ellsworth Kelly",
+"question": "1920, Growth of the Soil(1917), novel",
+"option1": "Elfriede Jelinek",
+"option2": "Mo Yan",
+"option3": "Knut Hamsun",
+"option4": "Derek Walcott",
 "answer": "3" }, 
                 
 {
-"question": "Premier disque, 1912",
-"option1": "Arshile Gorky",
-"option2": "Robert Motherwell",
-"option3": "Robert Delaunay",
-"option4": "Sam Francis",
+"question": "1996, Dlatego żyjemy-That is what we are living for(1952), poetry collection",
+"option1": "Jaroslav Seifert",
+"option2": "Kenzaburō Ōe",
+"option3": "Wisława Szymborska",
+"option4": "Elias Canetti",
 "answer": "3" },
                 
 {
-"question": "Autumn Rhythm, 1950",
-"option1": "Hans Arp",
-"option2": "Jackson Pollock",
-"option3": "Vasil'evič Kandinskij",
-"option4": "Josef Albers",
+"question": "1986, Aké: The Years of Childhood(1981), novel",
+"option1": "José Saramago",
+"option2": "Wole Soyinka",
+"option3": "Isaac Bashevis Singer",
+"option4": "Svetlana Alexievich",
 "answer": "2" },
                 
 {
-"question": "The Joy of Life, 1930",
-"option1": "Jackson Pollock",
-"option2": "Robert Delaunay",
-"option3": "Kazimir Malevič",
-"option4": "Paul Klee",
-"answer": "2" },
+"question": "1983, Lord of the flies(1954), novel ",
+"option1": "William Golding",
+"option2": "Isaac Bashevis Singer",
+"option3": "Tomas Tranströmer",
+"option4": "Derek Walcott",
+"answer": "1" },
                 
 {
-"question": "Yellow Island, 1952",
-"option1": "Juan Gris",
-"option2": "Jackson Pollock",
-"option3": "Georges Braque",
-"option4": "Robert Motherwell",
-"answer": "2" },
+"question": "1985, Les Géorgiques-The Georgics(1981), novel",
+"option1": "Elfriede Jelinek",
+"option2": "Imre Kertész",
+"option3": "Kenzaburō Ōe",
+"option4": "Claude Simon",
+"answer": "4" },
                 
 {
-"question": "Composition VII (the three graces), 1917",
-"option1": "Franz Kline",
-"option2": "Lee Krasner",
-"option3": "The van Doesburg",
-"option4": "Willem de Kooning",
+"question": "1988, The Day the Leader was Killed(1983), novel",
+"option1": "Jaroslav Seifert",
+"option2": "Mo Yan",
+"option3": "Naguib Mahfouz",
+"option4": "Elias Canetti",
 "answer": "3" },
                 
 {
-"question": "Orange and Yellow, 1956",
-"option1": "Mikhail Larionov",
-"option2": "Helen Frankenthaler",
-"option3": "Salvador Dali",
-"option4": "Mark Rothko",
+"question": "2007, The Good Terrorist(1985),novel",
+"option1": "Tomas Tranströmer",
+"option2": "Isaac Bashevis Singer",
+"option3": "Kenzaburō Ōe",
+"option4": "Doris Lessing",
 "answer": "4" },
 
 {
-"question": "Nulli Secundus, 1964",
-"option1": "Hans Hofmann",
-"option2": "Frank Stella",
-"option3": "Ellsworth Kelly",
-"option4": "Kenneth Noland",
+"question": "1997, Johan Padan and the Discovery of the Americas(1992),novel ",
+"option1": "Dario Fo",
+"option2": "Elfriede Jelinek",
+"option3": "Jaroslav Seifert",
+"option4": "Elias Canetti",
 "answer": "1" },
 
 
 {
-"question": "The Green Stripe, 1955",
-"option1": "Mark Rothko",
-"option2": "Jackosn Pollock",
-"option3": "Piet Mondrian",
-"option4": "Mikhail Larionov",
+"question": "2013, The View from Castle Rock(2006), collection of short stories",
+"option1": "Alice Munro",
+"option2": "Svetlana Alexievich",
+"option3": "Elfriede Jelinek",
+"option4": "Mo Yan",
 "answer": "1" }
 
 ]
